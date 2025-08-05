639498632edb9d4599484969b222a240b2b18ce4
vihir597 <vihirmhmd798@gmail.com>
2025-08-05 12:57:18 +0700

    Add files via upload

---
+++ tugas1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Moch Vihir Rakan Setiawan" << endl;
+    cout << "Cianjur 23-augt-2009" << endl;
+    cout << "kelas X MIPA"<< endl;
+   }
