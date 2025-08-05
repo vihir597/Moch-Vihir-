aa36eb8ab0f6994f543111f5cb51bf32a9d6ae66
vihir597 <vihirmhmd798@gmail.com>
2025-08-05 13:38:22 +0700

    Add files via upload

---
+++ tugas2 aplikasi penjumlahan.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout  <<"aplikasi penjumlahan";
+    cout  <<"masukan angka pertama";
+    int bi11,bi12,Hasil;
+    cin >> bi11;
+    cout <<"masukan angka kedua;";
+    cin >> bi12;
+    Hasil=bi11+bi12;
+    cout<<Hasil;
+    }
